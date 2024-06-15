a6257701aa8530c0a88c3eac6daab88341b1ea6a
Romanch JR <58005198+RomanchJR@users.noreply.github.com>
2024-06-15 17:32:50 +0545

    phase3.0

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-/*jslint browser:true */
-/*global $ */
+/* basic.css */
 
-$(document).ready(function () {
-	// Variables
-	const $body = $('body');
-	const $navbar = $('.navbar-default');
-	const $offsetY = $navbar.offset().top + 10;
-	const $menuButton = $('button.navbar-toggle');
-	const $menuIcon = $('.navbar-toggle .glyphicon');
-	const $collapsedMenuItem = $('.navbar-collapse.collapse li');
-	const $modalBackdropDiv = $('<div class="modal-backdrop fade in"></div>');
-	const $scrollButton = $('.scroll');
-	const $socialIcon = $('.social');
+body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+}
 
-	// Fixed Nav after scroll
-	function scroll() {
-		if ($(window).scrollTop() >= $offsetY) {
-			$navbar.addClass('menu-fixed').css('background-color', 'rgba(255,254,253,0.97)');
-		} else {
-			$navbar.removeClass('menu-fixed').css('background-color', 'transparent');
-		}
-	}
-	document.onscroll = scroll;
+.navbar {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: #333;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 1000;
+}
 
-	// Mobile Menu functions
-	function openMenu() {
-		$menuIcon.removeClass('glyphicon-menu-hamburger').addClass('glyphicon-remove active');
-		$modalBackdropDiv.css('z-index', 900);
-		$body.append($modalBackdropDiv);
-		if (!$navbar.hasClass('menu-fixed')) {
-			$navbar.css('background-color', 'rgba(255,254,253,0.97)');
-		}
-		// Close menu after clicking modal-backdrop
-		$modalBackdropDiv.on('click', function () {
-			$('.navbar-toggle').click();
-			closeMenu();
-		});
-	}
+.navbar-brand {
+    color: white;
+    font-size: 1.5em;
+}
 
-	function closeMenu() {
-		$menuIcon.removeClass('glyphicon-remove active').addClass('glyphicon-menu-hamburger');
-		$modalBackdropDiv.css('z-index', 1025).remove();
-		if (!$navbar.hasClass('menu-fixed')) {
-			$navbar.css('background-color', 'transparent');
-		}
-	}
+.navbar-menu {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
 
-	// Mobile Menu Icon Toggle
-	$menuButton.on('click', function () {
-		if ($menuIcon.hasClass('glyphicon-menu-hamburger')) {
-			openMenu();
-			// Close menu after clicking a link
-			$collapsedMenuItem.on('click', function () {
-				$('.navbar-toggle').click(); // Trigger collapse animation
-				closeMenu();
-			});
-		
+.navbar-menu li {
+    margin-left: 20px;
+}
+
+.navbar-menu li a {
+    color: white;
+    text-decoration: none;
+}
+
+.navbar-menu li a:hover {
+    text-decoration: underline;
+}
+
+.content {
+    padding-top: 60px; /* Add padding to account for the fixed navbar */
+}
+
+section {
+    padding: 20px;
+    border-bottom: 1px solid #ddd;
+}
+
+section h2 {
+    margin-top: 0;
+}
