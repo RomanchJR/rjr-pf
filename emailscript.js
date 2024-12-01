fa7d0dca6e4396bbee13b0cbcf272a5743adfd73
Romanch JR <58005198+RomanchJR@users.noreply.github.com>
2024-12-01 12:09:28 +0545

    change in email js

--- emailscript.js
+++ emailscript.js
@@ -1,29 +1,40 @@
-emailjs.init("RomJR");
-document.getElementById("contactForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent default form submission behavior
+// emailjs.init("RomJR");
+// document.getElementById("contactForm").addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent default form submission behavior
 
-    // Get form values
-    const formData = new FormData(this);
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const message = formData.get("message");
+//     // Get form values
+//     const formData = new FormData(this);
+//     const name = formData.get("name");
+//     const email = formData.get("email");
+//     const message = formData.get("message");
 
-    // Send email using EmailJS
-    emailjs
-        .send("RomJR", "romjr", {
-            name: name,
-            email: email,
-            message: message,
-        })
-        .then(
-            function (response) {
-                document.getElementById("responseMessage").innerHTML = 
-                  `<p style="color: green;">Message sent successfully!</p>`;
-            },
-            function (error) {
-                document.getElementById("responseMessage").innerHTML = 
-                  `<p style="color: red;">Failed to send message. Please try again later.</p>`;
-                console.error("EmailJS error:", error);
-            }
-        );
-});
+//     // Send email using EmailJS
+//     emailjs
+//         .send("RomJR", "romjr", {
+//             name: name,
+//             email: email,
+//             message: message,
+//         })
+//         .then(
+//             function (response) {
+//                 document.getElementById("responseMessage").innerHTML = 
+//                   `<p style="color: green;">Message sent successfully!</p>`;
+//             },
+//             function (error) {
+//                 document.getElementById("responseMessage").innerHTML = 
+//                   `<p style="color: red;">Failed to send message. Please try again later.</p>`;
+//                 console.error("EmailJS error:", error);
+//             }
+//         );
+// });
+
+
+function sendMail () {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    }
+
+    emailjs.send("RomJR")
+}
